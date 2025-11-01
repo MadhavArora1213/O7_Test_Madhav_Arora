7c1b3e72e5121469b6685c4614ec9d412a385cb9
Madhav Arora <madhavarora132005@gmail.com>
2025-11-01 10:56:38 +0530

    make just files not content inside and only craete the header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-
+import Header from './Components/Header.jsx'
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+// import { Link } from 'react-router-dom';
+function Header() {
+    return (
+        <>
+            <div className="bg-black opacity-80 text-white h-20 px-4 flex items-center border-b-2 border-gray-300 border-rounded">
+                <header>
+                    <div className="flex justify-around items-center w-[98.6vw]">
+                        <div className=''>
+                            <h1 className="text-3xl font-bold">Mini Order App</h1>
+                        </div>
+                        <div className='pr-20 flex justify-between items-center w-[25vw]'>
+                           <a href="#">Stationery Items</a>
+                           <a href="#">Printing Items</a>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        </>
+    )
+}
+
+export default Header
